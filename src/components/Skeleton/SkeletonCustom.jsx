3e31ab0d64605a07ea1849cac77c22fd06dbdd94
ShySannova <sandeep09prasad@gmail.com>
2023-06-14 15:15:12 +0530

    apk link added

---
+++ src/components/Skeleton/SkeletonCustom.jsx
@@ -0,0 +1,21 @@
+import "./SkeletonCustom.scss";
+
+
+const SkeletonCustom = () => {
+  return (
+        <div className="skeletonCard">
+            <div className="skeletonImg animate-pulse">
+
+            </div>
+            <div className="skeletonContent">
+              <div className="skeletonTitle animate-pulse"></div>
+              <div className="skeletonRating animate-pulse"></div>
+            </div>
+            <hr className="animate-pulse"/>
+            <div className="skeletonDesc animate-pulse"></div>
+            <div className="skeletonExtra animate-pulse"></div>
+        </div>
+  );
+};
+
+export default SkeletonCustom;
